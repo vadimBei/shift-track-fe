fd838bc6fd99e3f19c053eb7c2270bc7a9f295f3
Vadym Bei <bey1705@gmail.com>
2025-04-26 17:12:51 +0300

    refactored layout`s components

--- src/app/core/layout/components/layout/layout.component.ts
+++ src/app/core/layout/components/layout/layout.component.ts
@@ -1,9 +1,7 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from '../sidebar/sidebar.component';
-import { HeaderComponent } from '../header/header.component';
-import { AccountService } from '../../../account/services/account.service';
-import { CurrentUser } from '../../../account/models/current-user.model';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {SidebarComponent} from '../sidebar/sidebar.component';
+import {HeaderComponent} from '../header/header.component';
 
 @Component({
   selector: 'app-layout',
@@ -16,9 +14,5 @@ import { CurrentUser } from '../../../account/models/current-user.model';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent implements OnInit {
-
-  ngOnInit(): void {
-  }
-
+export class LayoutComponent {
 }
