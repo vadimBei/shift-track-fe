049a59b7c0159d31107c93d04e32f7839b74fd12
Vadym Bei <bey1705@gmail.com>
2025-04-26 16:36:18 +0300

    Refactor LoginPageComponent to improve form handling and cleanup
    Added OnDestroy lifecycle and unsubscribe pattern with `takeUntil` to prevent memory leaks. Updated form structure to strongly typed controls and revised template for better readability and maintainability. These changes enhance code clarity and improve component lifecycle management.

--- src/app/core/account/pages/login-page/login-page.component.ts
+++ src/app/core/account/pages/login-page/login-page.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, OnInit, OnDestroy, signal } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import { AccountService } from '../../services/account.service';
 import { Router } from '@angular/router';
 import { RouterLink } from '@angular/router';
-import { NgModule} from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
@@ -17,13 +17,25 @@ import { NgModule} from '@angular/core';
   styleUrl: './login-page.component.scss'
 })
 
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+
   private accountService = inject(AccountService);
   private router = inject(Router);
   private fb = inject(FormBuilder);
-  public form: FormGroup = new FormGroup({});
+
+  public form!: FormGroup<{
+    login: FormControl<string | null>;
+    password: FormControl<string | null>;
+  }>;
+
   isPasswordVisible = signal<boolean>(false);
   validationErrors?: string[];
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -46,6 +58,7 @@ export class LoginPageComponent implements OnInit {
   login() {
     if (this.form.valid) {
       this.accountService.login(this.form.value)
+        .pipe(takeUntil(this.destroy$))
         .subscribe({
           next: () => this.router.navigateByUrl('/employees/contact-list'),
           error: err => {
