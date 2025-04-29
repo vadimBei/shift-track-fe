44065b0cda37ba42a67f51d0851c6321bf5710e9
Vadym Bei <bey1705@gmail.com>
2025-04-29 11:12:51 +0300

    Refactor create-position-modal to use signals and OnDestroy
    Replaced `CreatePositionRequest` with reactive signals for state management and added `OnDestroy` to handle cleanup. Refactored form initialization for conciseness and updated RxJS subscription with `takeUntil` for proper unsubscription. Improved HTML formatting for better readability.

--- src/app/features/organization/structure/components/positions/create-position-modal/create-position-modal.component.ts
+++ src/app/features/organization/structure/components/positions/create-position-modal/create-position-modal.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { UnitService } from '../../../services/unit.service';
-import { BsModalRef } from 'ngx-bootstrap/modal';
-import { CreatePositionRequest } from '../../../models/create-position-request.model';
-import { PositionService } from '../../../services/position.service';
+import {Component, inject, OnDestroy, OnInit, signal} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {UnitService} from '../../../services/unit.service';
+import {BsModalRef} from 'ngx-bootstrap/modal';
+import {CreatePositionRequest} from '../../../models/create-position-request.model';
+import {PositionService} from '../../../services/position.service';
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-create-position-modal',
@@ -14,49 +15,58 @@ import { PositionService } from '../../../services/position.service';
   templateUrl: './create-position-modal.component.html',
   styleUrl: './create-position-modal.component.scss'
 })
-export class CreatePositionModalComponent implements OnInit {
-  fb = inject(FormBuilder);
-  positionService = inject(PositionService);
-  bsModalRef = inject(BsModalRef);
-  form: FormGroup = new FormGroup({});
-  request?: CreatePositionRequest;
+export class CreatePositionModalComponent implements OnInit, OnDestroy {
+  private readonly destroy$ = new Subject<void>();
 
-  ngOnInit(): void {
-    this.request = {
+  private readonly positionService = inject(PositionService);
+
+  bsModalRef = inject(BsModalRef);
+
+  fb = inject(FormBuilder);
+  form: FormGroup = this.fb.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.maxLength(100)
+      ]
+    ],
+    description: [
+      '',
+      [
+        Validators.required,
+        Validators.maxLength(100)
+      ]
+    ]
+  });
+
+  request = signal<CreatePositionRequest>(
+    {
       name: '',
       description: ''
     }
+  );
 
-    this.initializeForm();
+  ngOnInit(): void {
   }
 
-  initializeForm() {
-    this.form = this.fb.group({
-      name: [
-        '',
-        [
-          Validators.required,
-          Validators.maxLength(100)
-        ]
-      ],
-      description: [
-        '',
-        [
-          Validators.required,
-          Validators.maxLength(100)
-        ]
-      ]
-    });
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   createPosition() {
     if (!this.request)
       return;
 
-    this.request.name = this.form.value.name;
-    this.request.description = this.form.value.description;
+    this.request.update(value => ({
+      ...value,
+      name: this.form.value.name,
+      description: this.form.value.description
+    }));
 
-    this.positionService.createPosition(this.request!)
+    this.positionService.createPosition(this.request())
+      .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: (position) => {
           this.bsModalRef.hide();
