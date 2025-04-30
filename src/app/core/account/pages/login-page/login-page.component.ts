3ec523fa97f58d792d004f9b591bbb400c135d1b
Vadym Bei <bey1705@gmail.com>
2025-04-30 14:04:17 +0300

    Refactor LoginPageComponent to simplify form initialization.
    Removed OnInit lifecycle hook and moved form initialization directly into the property definition. This reduces code complexity and improves component readability.

--- src/app/core/account/pages/login-page/login-page.component.ts
+++ src/app/core/account/pages/login-page/login-page.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject, OnInit, OnDestroy, signal } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import { AccountService } from '../../services/account.service';
-import { Router } from '@angular/router';
-import { RouterLink } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
+import {Component, inject, OnDestroy, signal} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AccountService} from '../../services/account.service';
+import {Router} from '@angular/router';
+import {RouterLink} from '@angular/router';
+import {Subject, takeUntil} from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
@@ -17,17 +17,30 @@ import { Subject, takeUntil } from 'rxjs';
   styleUrl: './login-page.component.scss'
 })
 
-export class LoginPageComponent implements OnInit, OnDestroy {
+export class LoginPageComponent implements OnDestroy {
   private destroy$ = new Subject<void>();
 
   private accountService = inject(AccountService);
   private router = inject(Router);
   private fb = inject(FormBuilder);
 
-  public form!: FormGroup<{
-    login: FormControl<string | null>;
-    password: FormControl<string | null>;
-  }>;
+  public form: FormGroup = this.fb.group({
+    login:
+      [
+        '',
+        [
+          Validators.required
+        ]],
+    password:
+      [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(/^(?=.*\d)(?=.*[a-z]).{6,}$/)
+        ]
+      ]
+  });
 
   isPasswordVisible = signal<boolean>(false);
   validationErrors?: string[];
@@ -35,24 +48,6 @@ export class LoginPageComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
-  }
-
-  ngOnInit(): void {
-    this.initializeForm();
-  }
-
-  initializeForm() {
-    this.form = this.fb.group({
-      login: ['', [
-        Validators.required
-      ]],
-      password: ['', [
-        Validators.required,
-        Validators.minLength(6),
-        Validators.pattern(/^(?=.*\d)(?=.*[a-z]).{6,}$/)
-      ]
-      ]
-    });
   }
 
   login() {
